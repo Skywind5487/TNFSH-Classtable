0422e9de00144dfb16cf6bd83019f3d90704dcb7
Skywind5487 <skywind5487@gmail.com>
2025-06-05 14:37:04 +0800

    prebuild all

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 from tnfsh_class_table.interface import main_process
+from tnfsh_timetable_core.timetable.cache import preload_all
+import asyncio
+
+asyncio.run(preload_all(only_missing=False, max_concurrent=5, delay=0.01))  # 預載入所有課表資料
+
 main_process()
